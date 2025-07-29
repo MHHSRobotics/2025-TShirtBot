e8bf803761418b74fa6c7fccf906b3aedf6e61d1
ProgrammingtotheMax <leeimax.il@gmail.com>
2025-07-28 18:02:32 -0700

    Added more constants, made Turret constructor

--- src/main/subsystems/Turret.java
+++ src/main/subsystems/Turret.java
@@ -1,13 +1,32 @@
-import com.ctre.phoenix6.hardware.TalonFX;
+package frc.robot.subsystems;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 
 import static edu.wpi.first.units.Units.Radians;
+public class Turret{
 
-public class Turret {
     public static class Constants{
-        public static double minTurnAngle = Units.degreestoRadians(-180.0);
-        public static double maxTurnAngle = Units.degreestoRadians(180.0);
-    }
-    private TalonFX turretMotor;
+        public static final double minTurnAngle = Units.degreestoRadians(-180.0);
+        public static final double maxTurnAngle = Units.degreestoRadians(180.0);
+        public static final boolean motorInverted = true;
 
+        public static final double kP = 30;
+        public static final double kD = 10;
+
+        public static final double kS = 0.1;
+        public static final double kG = 1;
+        public static final double kV = 10;
+        public static final double kA = 1; 
+        
+    }
+
+    public Turret(){
+        TalonFXConfiguration turretMotor = new TalonFXConfiguration();
+        turretMotor.MotorOutputConfigs.Inverted = Constants.motorInverted ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+        
+        CANcoderConfiguration turretEncoder = new CANcoderConfiguration();
+        
+    }
+    
 }
