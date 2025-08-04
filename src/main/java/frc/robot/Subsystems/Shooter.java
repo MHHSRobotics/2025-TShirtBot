cd91ca46577098d088456eae84d85d6b5e98511d
Maxwell-Delatorre <maxwelldelatorre2009@gmail.com>
2025-08-04 16:19:11 -0700

    Shooter start

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -2,35 +2,43 @@ package frc.robot.Subsystems;
 
 import java.security.PrivateKey;
 
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenic6.configs.LoggedTalonFX;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
 
 public class Shooter extends SubsystemBase {
 
-    public static final int lowerMoterId = 21;
-    private LoggedTalonFX lowerMotor;
-    public static final int upperMoterId = 22;
-    private LoggedTalonFX upperMotor;
-    private CANcoderIO encoderIO;
+    public static final int maxSupplyCurrent = 40;
+
+    private final Spark lowerMotor = new Spark(8);
+    private final Spark upperMotor = new Spark(7);
+
+    private final Encoder lowerEncoder = new Encoder(0, 1);
+    private final Encoder upperEncoder = new Encoder(2, 3);
 
     public Shooter(){
-        
+
+        lowerMotor.setInverted(true);
+    //      lowerMotor.setSmartCurrentLimit(maxSupplyCurrent);
+    //    upperMotor.setSmartCurrentLimit(maxSupplyCurrent);
+
+
     }
     public void shoot(double speed) {
         lowerMotor.set(speed);
+        upperMotor.set(speed);
     }
 
     public void stop() {
-        
+        lowerMotor.set(0);
+        upperMotor.set(0);
     }
 
-    public void atSpeed() {
-        
-    }
-    public void periodic(){
-        SmartDashboard.putNumber(" upper RPM",upperEncoder.getVelocity());
-                SmartDashboard.putNumber(" lower RPM",lowerEncoder.getVelocity());
-
+    public boolean atSpeed() {
+        return Math.abs(upperEncoder.getRate()) > 5000 && Math.abs(lowerEncoder.getRate()) > 5000;
     }
 }
