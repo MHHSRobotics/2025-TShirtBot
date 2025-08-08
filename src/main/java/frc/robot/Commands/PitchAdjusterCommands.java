fb535b0fef35dffc112159e88274faf232dee294
Sej5137 <seijiro.dias1@outlook.com>
2025-08-07 17:42:58 -0700

    Cat removed, exterminated, deceased

--- src/main/java/frc/robot/Commands/PitchAdjusterCommands.java
+++ src/main/java/frc/robot/Commands/PitchAdjusterCommands.java
@@ -3,10 +3,10 @@ package frc.robot.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Subsystems.PitchCatjuster;
 
-public class PitchAdjusterCattands {
+public class PitchAdjusterCommands {
     PitchCatjuster pitchCatjuste =  new PitchCatjuster();
 
-    public PitchAdjusterCattands(PitchCatjuster pitchCatjuste){
+    public PitchAdjusterCommands(PitchCatjuster pitchCatjuste){
         this.pitchCatjuste = pitchCatjuste;
     }
     public InstantCommand setPitch(double angle){ //degree
