b6499167a1c75c8c3a997ed0d538692b01d90d6b
Sej5137 <seijiro.dias1@outlook.com>
2025-08-14 16:53:20 -0700

    Resolved Issues

--- src/main/java/frc/robot/subsystems/PitchAdjuster.java
+++ src/main/java/frc/robot/subsystems/PitchAdjuster.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -13,8 +12,6 @@ public class PitchAdjuster extends SubsystemBase {
         private static final int id = 1; // port
 
         // Motor constants
-        private static final double max = 1; // speed
-        private static final double min = 0; // speed
 
         private static final boolean inverted = false;
     }
@@ -30,8 +27,7 @@ public class PitchAdjuster extends SubsystemBase {
     }
 
     public void setSpeed(double speed) {
-        double clamped = MathUtil.clamp(speed, Constants.min, Constants.max);
-        motor.set(clamped);
+        motor.set(speed);
     }
 
     public void stop() {
